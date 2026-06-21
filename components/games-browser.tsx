95d0c2383078198bb3dea3110488554078ce516c
Darren <darren.chen4333@gmail.com>
2026-06-21 00:27:52 -0400

    add gap between filter panel and game grid

--- components/games-browser.tsx
+++ components/games-browser.tsx
@@ -141,7 +141,7 @@ export default function GamesBrowser({
           </button>
         </div>
       ) : (
-        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="mt-10 grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
           {visible.map(({ game, match }) => (
             <GameCard key={game.id} game={game} match={match} />
           ))}
